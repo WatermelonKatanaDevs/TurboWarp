e97c31fe633ebeb1e083a677992ba6b74dbd4005
Varrience <105261606+Varrience@users.noreply.github.com>
2025-02-27 13:56:43 -0500

    fix invisible unecessary third parameter

--- cdo-expo/index.js
+++ cdo-expo/index.js
@@ -5,7 +5,7 @@ const modules = {
 const requests = require("./requests");
 
 async function customExport(project) {
-  let type = (await requests.send(`https://studio.code.org/v3/channels/${project}`,"json",)).projectType;
+  let type = (await requests.send(`https://studio.code.org/v3/channels/${project}`,"json")).projectType;
   return (await modules[type].exportProject(project));
 }
 
