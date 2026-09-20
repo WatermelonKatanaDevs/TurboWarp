703aa8db5db8a6467b597a5d33f1549fd460caf3
Varrience <105261606+Varrience@users.noreply.github.com>
2026-09-20 06:15:43 -0400

    found new bug
    not sure how to fix but certian displays output wrong and produce a scrollbar

--- cdo-expo/ale.js
+++ cdo-expo/ale.js
@@ -102,8 +102,7 @@ async function getHTML(html, id, code) {
                   if(mutation.attributeName === "style") {
                     if((targetStyle.width !== width || targetStyle.height !== height) && targetStyle.transform !== "") {
                       targetStyle.transform = "";
-                    } else if((targetStyle.width === width && targetStyle.height === height) || targetStyle.position === "relative") {
-                      targetStyle.position = "absolute";
+                    } else if(targetStyle.width === width && targetStyle.height === height) {
                       if(targetStyle.transform === "") {
                         targetStyle.width = width;
                         targetStyle.height = height;
@@ -130,7 +129,7 @@ async function getHTML(html, id, code) {
     <link rel="stylesheet" href="${dependency}/CSS/style.css">
   </head>
   <body>
-  <div id="divApplab" class="appModern running" tabindex="1" style="position: absolute; top: 0px; left: 0px; width: 320px; height: 450px; display: block;">
+  <div id="divApplab" class="appModern running" tabindex="1" style="width: 320px; height: 450px; display: block;">
   <button id="BBA1E1582A51E627F185162F66A03477" style="display: none; position: fixed"></button>
   ${html.match(/<div[^>]*class="screen".*/g)[0]}
   </body>
